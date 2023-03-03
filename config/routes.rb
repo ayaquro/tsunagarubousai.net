5b8fb53986c5f6c32fba4cd38029b4238e12e613
ayaquro <121092738+ayaquro@users.noreply.github.com>
2023-03-03 05:49:51 +0000

    [Add]controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get 'posts/new'
+    get 'posts/index'
+    get 'posts/edit'
+    get 'posts/show'
+  end
+  namespace :public do
+    get 'homes/top'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
