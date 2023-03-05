c386a457e6a0a16bbedc1135b7f52a7321d84608
ayaquro <121092738+ayaquro@users.noreply.github.com>
2023-03-05 11:07:00 +0000

    [Add]退会機能追加/enum導入

--- app/controllers/general_user/sessions_controller.rb
+++ app/controllers/general_user/sessions_controller.rb
@@ -18,7 +18,21 @@ class GeneralUser::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
+  #退会しているかを判断するメソッド
+  def general_user_state
+    ## 【処理内容1】 入力されたemailからアカウントを1件取得
+    @general_user = GeneralUser.find_by(email: params[:general_user][:email])
+    ## アカウントを取得できなかった場合、このメソッドを終了する
+    return if !@general_user
+    ## 【処理内容2】 取得したアカウントのパスワードと入力されたパスワードが一致してるかを判別
+    if @general_user.valid_password?(params[:general_user][:password])
+      ## 【処理内容3】登録ステータスがregisteringではなかったら、新規登録画面に遷移
+      if @general_user.registration_status!="registering"
+         redirect_to new_general_user_registration_path
+      end
+    end
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module TsunagarubousaiNet
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    #Railsアプリケーションのデフォルトの言語を日本語にするため
+    config.i18n.default_locale = :ja
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
