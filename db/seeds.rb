10aea501ce867a896ddfc0c86fe56ac085384497
ayaquro <121092738+ayaquro@users.noreply.github.com>
2023-03-25 12:26:11 +0000

    seedファイル追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,15 +14,15 @@ Staff.create!(
 
 general_users=GeneralUser.create!(
   [
-    {last_name: '防災', first_name: '守子', kana_last_name: 'ボウサイ', kana_first_name: 'マコ', email: 'bousai@mako', encrypted_password: 'bousai', is_deleted: "false", profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-userw1.jpg"), filename:"sample-userw1.jpg")},
-    {last_name: '防災', first_name: '守美', kana_last_name: 'ボウサイ', kana_first_name: 'マミ', email: 'bousai@mami', encrypted_password: 'bousai', is_deleted: "false", profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-userw2.jpg"), filename:"sample-userw2.jpg")},
-    {last_name: '地域', first_name: '守', kana_last_name: 'チイキ', kana_first_name: 'マモル', email: 'chiiki@mamoru', encrypted_password: 'bousai', is_deleted: "false", profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-userm1.jpg"), filename:"sample-userm1.jpg")},
-    {last_name: '地域', first_name: '守朗', kana_last_name: 'チイキ', kana_first_name: 'マモロウ', email: 'chiiki@mamorou', encrypted_password: 'bousai', is_deleted: "false", profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-userm2.jpg"), filename:"sample-userm2.jpg")}
+    {last_name: '防災', first_name: '守子', kana_last_name: 'ボウサイ', kana_first_name: 'マコ', email: 'bousai@mako', password: 'bousai', is_deleted: "false", profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-userw1.jpg"), filename:"sample-userw1.jpg")},
+    {last_name: '防災', first_name: '守美', kana_last_name: 'ボウサイ', kana_first_name: 'マミ', email: 'bousai@mami', password: 'bousai', is_deleted: "false", profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-userw2.jpg"), filename:"sample-userw2.jpg")},
+    {last_name: '地域', first_name: '守', kana_last_name: 'チイキ', kana_first_name: 'マモル', email: 'chiiki@mamoru', password: 'bousai', is_deleted: "false", profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-userm1.jpg"), filename:"sample-userm1.jpg")},
+    {last_name: '地域', first_name: '守朗', kana_last_name: 'チイキ', kana_first_name: 'マモロウ', email: 'chiiki@mamorou', password: 'bousai', is_deleted: "false", profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-userm2.jpg"), filename:"sample-userm2.jpg")}
   ]
   )
 
 5.times do |n|
   District.create!(
-    district_name: '福島#{n+1}',
+    district_name: "福島#{n+1}",
   )
 end
