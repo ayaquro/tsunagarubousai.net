fcb1a52e9cf556fc144edb9db9f1f8225abedc0f
ayaquro <121092738+ayaquro@users.noreply.github.com>
2023-03-26 12:05:29 +0000

    ゲストユーザー退会不可実装

--- app/controllers/general_user/general_users_controller.rb
+++ app/controllers/general_user/general_users_controller.rb
@@ -29,10 +29,14 @@ class GeneralUser::GeneralUsersController < ApplicationController
   def unsubscribe
     @general_user = current_general_user
     #is_deletedカラムをtrueに変更することにより削除フラグを立てる
-    @general_user.update(is_deleted: true)
-    reset_session
-    flash[:notice] = "退会処理を実行しました"
-    redirect_to root_path
+    if @general_user.last_name == "ゲスト"
+      redirect_to general_user_path(current_general_user), notice: "ゲストユーザーは退会できません。"
+    else
+      @general_user.update(is_deleted: true)
+      reset_session
+      flash[:notice] = "退会処理を実行しました。"
+      redirect_to root_path
+    end
   end
 
   private
